9414b457e947bb5fd381a487ab2ac22163091a5d
shinyay <shinya.com@gmail.com>
2022-02-28 22:07:38 +0900

    Add reactive mongo db dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,9 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
