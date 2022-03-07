8084d2729138ea8f3942e649b8200f306755dabb
shinyay <shinya.com@gmail.com>
2022-03-07 17:19:46 +0900

    Modify type as variable number of arguments

--- src/main/kotlin/io/spring/reactive/service/CustomerService.kt
+++ src/main/kotlin/io/spring/reactive/service/CustomerService.kt
@@ -3,7 +3,7 @@ package io.spring.reactive.service
 import io.spring.reactive.entity.Customer
 
 interface CustomerService {
-    fun save(names: String): Collection<Customer>
+    fun save(vararg names: String): Collection<Customer>
     fun findById(id: Long): Customer
     fun findAll(): Collection<Customer>
 }
