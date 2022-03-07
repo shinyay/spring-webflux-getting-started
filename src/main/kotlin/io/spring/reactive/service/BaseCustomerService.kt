c5bb77ef2514b5464afa641d277135fe5cb6fbd1
shinyay <shinya.com@gmail.com>
2022-03-07 16:56:05 +0900

    Add RowMapper to operate with JdbcTemplate

--- src/main/kotlin/io/spring/reactive/service/BaseCustomerService.kt
+++ src/main/kotlin/io/spring/reactive/service/BaseCustomerService.kt
@@ -6,7 +6,11 @@ import java.sql.ResultSet
 
 class BaseCustomerService(
     val jdbcTemplate: JdbcTemplate) : CustomerService {
-
+    val rorMapper = {rs: ResultSet, i: Int ->
+        Customer(
+            rs.getLong("id"),
+            rs.getString("name"))
+    }
 
     override fun save(names: String): Collection<Customer> {
         TODO("Not yet implemented")
