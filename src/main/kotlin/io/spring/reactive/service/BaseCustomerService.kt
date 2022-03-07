a1b4fe40c2f27a3ff579b9b220b5f0644f806007
shinyay <shinya.com@gmail.com>
2022-03-07 17:37:58 +0900

    Add save function implemented

--- src/main/kotlin/io/spring/reactive/service/BaseCustomerService.kt
+++ src/main/kotlin/io/spring/reactive/service/BaseCustomerService.kt
@@ -2,22 +2,38 @@ package io.spring.reactive.service
 
 import io.spring.reactive.entity.Customer
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.support.GeneratedKeyHolder
 import java.sql.ResultSet
+
 
 class BaseCustomerService(
     val jdbcTemplate: JdbcTemplate) : CustomerService {
-    val rorMapper = {rs: ResultSet, i: Int ->
+    val rowMapper = {rs: ResultSet, i: Int ->
         Customer(
             rs.getLong("id"),
             rs.getString("name"))
     }
 
-    override fun save(names: String): Collection<Customer> {
-        TODO("Not yet implemented")
+    override fun save(vararg names: String): Collection<Customer> {
+        val customerList = mutableListOf<Customer>()
+        for (name in names) {
+            val keyHolder = GeneratedKeyHolder()
+            jdbcTemplate.update({
+                connection ->
+                val ps = connection.prepareStatement("insert into CUSTOMERS (name) values (?)")
+                ps.setString(1, name)
+                ps
+            }, keyHolder)
+            val keyHolderKey = keyHolder.key?.toLong()
+            val customer = this.findById(keyHolderKey!!)
+            customerList.add(customer)
+        }
+        return customerList
     }
 
-    override fun findById(id: Long): Customer {
-        TODO("Not yet implemented")
+    fun findById(id: Long?): Customer? {
+        val sql = "select * from CUSTOMERS where id = ?"
+        return jdbcTemplate.queryForObject(sql, this.rowMapper, id)
     }
 
     override fun findAll(): Collection<Customer> {
