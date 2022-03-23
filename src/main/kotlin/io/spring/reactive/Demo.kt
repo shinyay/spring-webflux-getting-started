099d156c9b29a961de707cff80f71f0cadf07e66
shinyay <shinya.com@gmail.com>
2022-03-23 23:29:03 +0900

    Modify function with companion object

--- src/main/kotlin/io/spring/reactive/Demo.kt
+++ src/main/kotlin/io/spring/reactive/Demo.kt
@@ -4,12 +4,14 @@ import io.spring.reactive.service.CustomerService
 
 class Demo {
 
-    fun work(customerService: CustomerService) {
-        customerService.findAll()?.forEach {
-            customer -> {
-                val customerId = customer.id
-                val customerById = customerService.findById(customerId)
-                println("Found: $customerById")
+    companion object {
+        fun work(customerService: CustomerService) {
+            customerService.findAll()?.forEach { customer ->
+                {
+                    val customerId = customer.id
+                    val customerById = customerService.findById(customerId)
+                    println("Found: $customerById")
+                }
             }
         }
     }
